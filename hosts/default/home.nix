7328e03774abf2aa1201bd1f65e69ff1b8f13e39
Ethan Perruzza <ethan.perruzza@epita.fr>
2024-07-04 19:42:51 +0200

    feat: brightness control

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -25,6 +25,7 @@
     kitty
     dolphin
     wofi
+    brightnessctl
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -141,6 +142,8 @@
         ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
         ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
         ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+        ", XF86MonBrightnessUp, exec, brightnessctl set 5%+" 
       ];
 
     exec-once = ''
